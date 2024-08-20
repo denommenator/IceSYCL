101281a130e888e96946fba95f2eeb762d9d61e9
robert-denomme <robert.a.denomme@gmail.com>
2024-08-20 16:14:50 -0400

    Implement segment_id function

--- include/IceSYCL/particle_node_interactions.hpp
+++ include/IceSYCL/particle_node_interactions.hpp
@@ -17,6 +17,82 @@
 namespace iceSYCL
 {
 
+
+template<class Input_t, class BinaryPred>
+    void id_segments(
+        sycl::queue& q,
+        sycl::buffer<Input_t>& inputs,
+        sycl::buffer<size_t>& is_segment_begin,
+        sycl::buffer<size_t>& segment_ids,
+        BinaryPred p
+    )
+{
+    q.submit([&](sycl::handler& h)
+    {
+        sycl::accessor is_segment_begin_acc(is_segment_begin, h);
+        h.single_task([=](){is_segment_begin_acc[0] = 0;});
+    });
+
+    q.submit([&](sycl::handler& h)
+    {
+        sycl::accessor inputs_acc(inputs, h);
+        sycl::accessor is_segment_begin_acc(is_segment_begin, h);
+
+        h.parallel_for(inputs.size() - 1, [=](sycl::id<1> idx)
+        {
+            bool is_same = p(inputs_acc[idx[0]], inputs_acc[idx[0] + 1]);
+            is_segment_begin_acc[idx[0] + 1] = is_same ? 0 : 1;
+        });
+    });
+
+
+    dpl::inclusive_scan(dpl::execution::make_device_policy(q),
+        dpl::begin(is_segment_begin),
+        dpl::end(is_segment_begin),
+        dpl::begin(segment_ids),
+        std::plus<size_t>{}
+        );
+
+    q.submit([&](sycl::handler& h)
+    {
+        sycl::accessor is_segment_begin_acc(is_segment_begin, h);
+        h.single_task([=](){is_segment_begin_acc[0] = 1;});
+    });
+}
+
+template<class ExecutionPolicy, class InputIt, class OutputIt, class BinaryPred>
+    void segment_id(
+        ExecutionPolicy exec,
+        InputIt first,
+        InputIt last,
+        OutputIt d_first,
+        BinaryPred p
+    )
+{
+    auto are_different_pair = [=](typename std::iterator_traits<InputIt>::value_type a, typename std::iterator_traits<InputIt>::value_type b)->size_t
+    {
+        return p(a, b) ? 0 : 1;
+    };
+
+    oneapi::dpl::zip_iterator adjacent = dpl::make_zip_iterator(first, dpl::next(first));
+    using zip_t = decltype(adjacent);
+
+    auto are_different_zip = [=](typename std::iterator_traits<zip_t>::value_type zipped_adjacent)->size_t
+    {
+        return are_different_pair(oneapi::dpl::get<0>(zipped_adjacent), oneapi::dpl::get<1>(zipped_adjacent));
+    };
+
+    oneapi::dpl::transform_iterator are_different_it = oneapi::dpl::make_transform_iterator(adjacent, are_different_zip);
+
+    dpl::exclusive_scan(
+        exec,
+        are_different_it,
+        are_different_it + (last - first),
+        d_first,
+        0
+    );
+}
+
 template<class TInterpolationScheme>
 class ParticleNodeInteractionManager
 {
@@ -27,15 +103,32 @@ public:
     using NodeIndex_t = typename CoordinateConfiguration::NodeIndex_t;
     using Coordinate_t = typename CoordinateConfiguration::Coordinate_t;
 
+    struct NodeData
+    {
+        NodeIndex_t node_index;
+        size_t particle_interaction_begin;
+        size_t particle_interaction_count;
+    };
+
     ParticleNodeInteractionManager(const size_t particle_count) :
     particle_count_{particle_count},
     interactions_by_particle_{particle_count},
-    interactions_by_node_{particle_count}
-    {}
+    interactions_by_node_{particle_count},
+    node_id_by_node_interaction{particle_count},
+    segment_begin{particle_count}
+    {
+        sycl::host_accessor segment_begin_acc(segment_begin);
+        std::fill(oneapi::dpl::begin(segment_begin_acc), oneapi::dpl::end(segment_begin_acc), 0);
+    }
 
     size_t particle_count_;
     sycl::buffer<ParticleNodeInteraction<CoordinateConfiguration>> interactions_by_particle_;
     sycl::buffer<ParticleNodeInteraction<CoordinateConfiguration>> interactions_by_node_;
+
+    sycl::buffer<size_t> segment_begin;
+    sycl::buffer<size_t> node_id_by_node_interaction;
+
+
 
     void generate_particle_node_interactions(
         sycl::queue q,
@@ -66,15 +159,51 @@ public:
 
 
 
-        auto dpl_policy = dpl::execution::make_device_policy<class SortPolicy>(q);
+        auto dpl_policy = dpl::execution::make_device_policy<class ParticleNodeInteractionGenerationPolicy>(q);
 
+        auto node_index_comparer = [](NodeIndex_t& a, NodeIndex_t& b)->bool
+        {
+            for(size_t dim = 0; dim < CoordinateConfiguration::Dimension; dim++)
+            {
+                if(a(dim) < b(dim))
+                    return true;
+                else if(a(dim) > b(dim))
+                    return false;
+            }
+            return false;
+        };
+
+        auto interaction_comparer = [=](ParticleNodeInteraction<CoordinateConfiguration>& a, ParticleNodeInteraction<CoordinateConfiguration>& b)->bool
+        {
+            return node_index_comparer(a.node_index, b.node_index);
+        };
 
         oneapi::dpl::copy(dpl_policy,
             dpl::begin(interactions_by_particle_),
             dpl::end(interactions_by_particle_),
             dpl::begin(interactions_by_node_));
 
+        /*
+        oneapi::dpl::sort(dpl_policy,
+            dpl::begin(interactions_by_node_),
+            dpl::end(interactions_by_node_),
+            interaction_comparer
+        );
+
+
+
+
+        segment_id(dpl_policy,
+            dpl::begin(interactions_by_node_),
+            dpl::end(interactions_by_node_),
+            node_id_by_node_interaction,
+            is_different_node_in_interaction);
+        */
+
+
     }
+
+
 
     size_t get_node_count()
     {
@@ -95,7 +224,38 @@ private:
             };
         return ret;
     }
+/*
+    void mark_interaction_by_node_segments(sycl::queue q)
+    {
+        auto is_equal = [](NodeIndex_t& a, NodeIndex_t& b)->bool
+        {
+            for(size_t dim = 0; dim < CoordinateConfiguration::Dimension; dim++)
+            {
+                if(a(dim) != b(dim))
+                    return false;
+            }
+            return true;
+        };
 
+        auto is_different_node_in_interaction = [=](ParticleNodeInteraction<CoordinateConfiguration>& a, ParticleNodeInteraction<CoordinateConfiguration>& b)->size_t
+        {
+            return is_equal(a.node_index, b.node_index) ? 0 : 1;
+        };
+
+        q.submit([&](sycl::handler h)
+        {
+            sycl::accessor interactions_by_node_acc(interactions_by_node_, h);
+            sycl::accessor segment_begin_acc(segment_begin, h);
+
+            q.parallel_for(particle_count_ - 1, [=](sycl::id<1> idx)
+            {
+
+            });
+
+        });
+
+    }
+*/
 };
 
 }
--- tests/particle_node_interactions.cpp
+++ tests/particle_node_interactions.cpp
@@ -4,12 +4,13 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-#include <sycl/sycl.hpp>
 
 #include <oneapi/dpl/execution>
 #include <oneapi/dpl/iterator>
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/ranges>
+
+#include <sycl/sycl.hpp>
 
 #include <IceSYCL/interpolation.hpp>
 #include <IceSYCL/particle_node_interactions.hpp>
@@ -123,54 +124,220 @@ TEST_CASE( "ranges zip test", "[particle_node_interactions]" )
 
 }
 
-/*
-TEST_CASE( "inclusive scan device test", "[particle_node_interactions]" )
+TEST_CASE( "drop view write test", "[particle_node_interactions]" )
 {
-    constexpr size_t count = 10;
-    std::array<int, count> vec_list = {0, 0, 0, 1, 0, 0, 1, 1, 1, 0};
-    std::array<size_t, count> segment_ids;
-    segment_ids[0] = 0;
-    std::array<size_t, count> segment_ids_desired = {0, 0, 0, 1, 1, 1, 2, 3, 4, 4};
-
-    namespace rng = dpl::experimental::ranges;
-    namespace views = oneapi::dpl::experimental::ranges::views;
+    std::array<int, 11> data = {1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 7};
+    std::array<int, 5> result;
     {
+        namespace rng = dpl::experimental::ranges;
+        sycl::buffer dataB(data);
+        sycl::buffer resultB(result);
 
-        sycl::buffer vec_listB(vec_list);
-        sycl::buffer segment_idsB(segment_ids);
+        auto data_view = rng::views::all_read(dataB);
+        auto result_view = rng::views::all_write(resultB);
 
-        auto range_0 = rng::take_view(views::all_read(vec_listB), count - 1);
-        auto range_1 = rng::drop_view(views::all_read(vec_listB), 1);
+        auto data_drop_view = data_view | rng::views::drop(1);
+        auto result_drop_view = result_view | rng::views::drop(1);
 
-        auto zip_view = dpl::experimental::ranges::zip_view(range_0, range_1);
+        //auto adjacent_view = rng::zip_view(data_view, data_view | rng::views::drop(1));
+        auto adjacent_view = rng::zip_view(rng::views::all_read(dataB), rng::views::all_read(dataB) | rng::views::drop(1));
+        //hack to read from drop view of input!
 
-        auto is_different = [](const int& a, const int& b)->size_t{return a == b ? 1 : 0;};
-        auto is_different_pair = [=](std::tuple<const int&, const int&> z)->size_t{return is_different(std::get<0>(z), std::get<1>(z));};
-        //rng::transform(dpl::execution::dpcpp_default,
-        //    zip_view,
-        //    views::all_write(segment_ids) | views::drop(1),
-        //    is_different_pair
-        //    );
-//
-        //rng::inclusive_scan(dpl::execution::dpcpp_default,
-        //    );
+        auto get_second = [](std::tuple<const int&, const int&> x){return std::get<1>(x);};
+        //auto direct_drop_view = rng::drop_view(data_view, 1);
+        //rng::copy(dpl::execution::dpcpp_default, adjacent_view, get_second);
+        //rng::copy(dpl::execution::dpcpp_default, data_drop_view, result_view);
+        //rng::copy(dpl::execution::dpcpp_default, data_view, result_drop_view);
 
-        rng::transform_inclusive_scan(dpl::execution::dpcpp_default,
-            zip_view,
-            views::all_write(segment_ids) | views::drop(1),
-            std::plus<size_t>{},
-            is_different_pair,
-            0
-        );
-
-    }
-
-    for(auto seg_id = segment_ids.begin(), seg_id_desired = segment_ids_desired.begin(); seg_id != segment_ids.end(); ++seg_id, ++seg_id_desired)
-    {
-        REQUIRE(*seg_id == *seg_id_desired);
+        rng::transform(dpl::execution::dpcpp_default, adjacent_view, rng::views::all_write(resultB), get_second);
     }
 }
-*/
+// //seemingly you can't write to a drop view!!
+// TEST_CASE( "ranges adjacent zip test", "[particle_node_interactions]" )
+// {
+//     std::vector<int> dataA = {1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 7};
+//     std::vector<int> result = dataA;
+//
+//     {
+//         namespace rng = dpl::experimental::ranges;
+//         namespace views = rng::views;
+//
+//         sycl::buffer bufferA(dataA);
+//         sycl::buffer resultB(result);
+//
+//         auto zip = rng::zip_view(rng::views::all_read(bufferA), views::all_read(bufferA) | views::drop(1));
+//         rng::transform(dpl::execution::dpcpp_default,
+//             zip,
+//             rng::drop_view(rng::all_view<int, sycl::access::mode::write>(resultB), 1),
+//             [](std::tuple<const int&, const int&> el){return std::get<1>(el) - std::get<0>(el);}
+//             );
+//         //rng::all_view vec_list_range(vec_listB);
+//         //rng::all_view result_range(resultB);
+//         //rng::copy(dpl::execution::dpcpp_default, rng::views::all_read(vec_listB), rng::views::all_write(resultB));
+//         //rng::transform(dpl::execution::dpcpp_default, rng::views::all_read(vec_listB), rng::views::all_write(resultB), [](const int& x){return x+1;});
+//     }
+//
+//     for(int i = 0; i < result.size(); i++)
+//     {
+//         std::cout << result[i] << ", ";
+//         //REQUIRE(result[i] == dataA[i] + dataB[i]);
+//     }
+//     std::cout << std::endl;
+// }
+//
+// TEST_CASE( "ranges segment begin test", "[particle_node_interactions]" )
+// {
+//     constexpr size_t count = 10;
+//     std::array<int, count> vec_list = {0, 0, 0, 1, 0, 0, 1, 1, 1, 0};
+//     std::array<bool, count> is_segment_begin;
+//     std::array<size_t, count> segment_ids;
+//     //is_segment_begin[0] = ;
+//     std::array<size_t, count> segment_ids_desired = {0, 0, 0, 1, 1, 1, 2, 3, 4, 4};
+//
+//     namespace rng = dpl::experimental::ranges;
+//     namespace views = rng::views;
+//     {
+//         sycl::buffer vec_listB(vec_list);
+//         sycl::buffer segment_idsB(segment_ids);
+//         sycl::buffer is_segment_beginB(is_segment_begin);
+//
+//         auto adjacent_view = rng::zip_view(views::all_read(vec_listB), views::all_read(vec_listB) | views::drop(1));
+//
+//         auto is_different = [](std::tuple<const int&, const int&> el){return std::get<0>(el) != std::get<1>(el);};
+//
+//         rng::transform(dpl::execution::dpcpp_default,
+//             adjacent_view,
+//             views::all_write(is_segment_beginB),// | views::drop(1),
+//             is_different
+//         );
+//
+//         auto bool_to_size_t = [](const bool& b)->size_t{return b ? 1 : 0;};
+//
+//         rng::transform_inclusive_scan(dpl::execution::dpcpp_default,
+//             views::all_read(is_segment_beginB),
+//             views::all_write(segment_idsB),
+//             0,
+//             std::plus<size_t>{},
+//             bool_to_size_t
+//             );
+//     }
+//
+//     for(int i = 0; i < count; i++)
+//     {
+//         std::cout << is_segment_begin[i]  << ", ";
+//     }
+//     std::cout << std::endl;
+//     for(int i = 0; i < count; i++)
+//     {
+//         std::cout << segment_ids[i] << ", ";
+//     }
+//
+//     std::cout << std::endl;
+// }
+
+
+TEST_CASE( "inclusive scan device test", "[particle_node_interactions]" )
+{
+
+    std::vector<int> vec_list = {1, 1, 1, 2, 2, 3, 4, 5, 6, 6, 6, 7};
+    const size_t count = vec_list.size();
+    std::vector<bool> is_segment_begin(count, false);
+    std::vector<size_t> segment_ids(count, 0);
+    segment_ids[0] = 0;
+    std::vector<size_t> segment_ids_desired = {0,0,0,1,1,2,3,4,5,5,5,6};
+
+    auto is_different = [](const int& x, const int& y){return x != y;};
+    std::transform(vec_list.begin(), std::prev(vec_list.end()), std::next(vec_list.begin()), std::next(is_segment_begin.begin()), is_different);
+    auto bool_to_size_t = [](const bool& b)->size_t{return b ? 1 : 0;};
+    std::transform_inclusive_scan(is_segment_begin.begin(), is_segment_begin.end(), segment_ids.begin(), std::plus<size_t>{}, bool_to_size_t, 0);
+
+    for(size_t i = 0; i < vec_list.size(); ++i)
+    {
+        //std::cout << segment_ids[i] << ", ";
+        REQUIRE(segment_ids[i] == segment_ids_desired[i]);
+    }
+    //std::cout << std::endl;
+//
+//     namespace rng = dpl::experimental::ranges;
+//     namespace views = oneapi::dpl::experimental::ranges::views;
+//     {
+//
+//         sycl::buffer vec_listB(vec_list);
+//         sycl::buffer segment_idsB(segment_ids);
+//
+//         auto range_0 = rng::take_view(views::all_read(vec_listB), count - 1);
+//         auto range_1 = rng::drop_view(views::all_read(vec_listB), 1);
+//
+//         auto zip_view = dpl::experimental::ranges::zip_view(range_0, range_1);
+//
+//         auto is_different = [](const int& a, const int& b)->size_t{return a == b ? 1 : 0;};
+//         auto is_different_pair = [=](std::tuple<const int&, const int&> z)->size_t{return is_different(std::get<0>(z), std::get<1>(z));};
+//         //rng::transform(dpl::execution::dpcpp_default,
+//         //    zip_view,
+//         //    views::all_write(segment_ids) | views::drop(1),
+//         //    is_different_pair
+//         //    );
+// //
+//         //rng::inclusive_scan(dpl::execution::dpcpp_default,
+//         //    );
+//
+//         rng::transform_inclusive_scan(dpl::execution::dpcpp_default,
+//             zip_view,
+//             views::all_write(segment_ids) | views::drop(1),
+//             std::plus<size_t>{},
+//             is_different_pair,
+//             0
+//         );
+//
+//     }
+//
+//     for(auto seg_id = segment_ids.begin(), seg_id_desired = segment_ids_desired.begin(); seg_id != segment_ids.end(); ++seg_id, ++seg_id_desired)
+//     {
+//         REQUIRE(*seg_id == *seg_id_desired);
+//     }
+}
+
+TEST_CASE( "id_segments function test", "[particle_node_interactions]" )
+{
+    std::vector<int> vec_list = {1, 1, 1, 2, 2, 3, 4, 5, 6, 6, 6, 7};
+    const size_t count = vec_list.size();
+    std::vector<size_t> is_segment_begin(count, 0);
+    std::vector<size_t> segment_ids(count, 0);
+    std::vector<size_t> is_segment_begin_desired = {1,0,0,1,0,1,1,1,1,0,0,1};
+    std::vector<size_t> segment_ids_desired = {0,0,0,1,1,2,3,4,5,5,5,6};
+
+    {
+        sycl::queue q(sycl::gpu_selector_v);
+        sycl::buffer vec_listB(vec_list);
+        sycl::buffer is_segment_beginB(is_segment_begin);
+        sycl::buffer segment_idsB(segment_ids);
+
+        auto pred = [](const int& a, const int& b){return a == b;};
+
+        iceSYCL::id_segments(q,
+            vec_listB,
+            is_segment_beginB,
+            segment_idsB,
+            pred);
+
+    }
+
+    for(size_t i = 0; i < vec_list.size(); ++i)
+    {
+        //std::cout << is_segment_begin[i] << ", ";
+        REQUIRE(is_segment_begin[i] == is_segment_begin_desired[i]);
+    }
+    //std::cout << std::endl;
+
+    for(size_t i = 0; i < vec_list.size(); ++i)
+    {
+        //std::cout << segment_ids[i] << ", ";
+        REQUIRE(segment_ids[i] == segment_ids_desired[i]);
+    }
+    //std::cout << std::endl;
+}
+
+
 
 TEST_CASE( "segment_id logic test", "[particle_node_interactions]" )
 {
